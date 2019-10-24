d6fbba70b03731b3a6ebab367ce4445279cdd801
Eden5Wu <yrw565@yahoo.com.tw>
2019-10-24 09:22:29 +0800

    Add files via upload

--- DBExpress.MSSQL.Factory.pas
+++ DBExpress.MSSQL.Factory.pas
@@ -21,9 +21,11 @@ type
     function Execute(ASQL: string): Int64; overload;
     function Execute(ASQL: string; args: OleVariant): Int64; overload;
     function Execute(ASQLCommand: TDBXCommand): Int64; overload;
-    function ExecuteQuery(ASQLCommand: TDBXCommand): TDBXReader;
-    function Prepare(ASQL: string): TDBXCommand; overload;
-    function Prepare(ASQL: string; args: OleVariant): TDBXCommand; overload;
+    function Execute(ASQLCommand: TDBXCommand; args: OleVariant): Int64; overload;
+    function ExecuteQuery(ASQLCommand: TDBXCommand): TDBXReader; overload;
+    function ExecuteQuery(ASQL: string; args: OleVariant): TDBXReader; overload;
+    function ExecuteQuery(ASQLCommand: TDBXCommand; args: OleVariant): TDBXReader; overload;
+    function Prepare(ASQL: string): TDBXCommand;
     property ConnectionProps: TDBXProperties read FConnectionProps;
   end;
 
@@ -36,14 +38,84 @@ uses
 const DEVART_MSSQL_CONNECTION_STRING = ''
   +'%s=DevartSQLServer;%s=%s;%s=%s;%s=%s;%s=%s'
   +';BlobSize=-1;SchemaOverride=%%.dbo;LongStrings=True;EnableBCD=True'
-  //+';Custom String=ApplicationName=A_NAME;WorkstationID=WID'
   +';FetchAll=True;UseUnicode=True;IPVersion=IPv4';
+  //+';Custom String=ApplicationName=A_NAME;WorkstationID=WID';
 // uses DbxMsSql
 const MSSQL_CONNECTION_STRING = ''
   +'%s=MSSQL;%s=%s;%s=%s;%s=%s;%s=%s'
   +';SchemaOverride=%%.dbo;BlobSize=-1;ErrorResourceFile=;LocaleCode=0000'
   +';IsolationLevel=ReadCommitted;OS Authentication=False;Prepare SQL=True'
   +';ConnectTimeout=60;Mars_Connection=False';
+/// <summary> fixed: Cursor not returned from Query </summary>
+const SET_NOCOUNT_ON = 'SET NOCOUNT ON;'#13#10;
+
+procedure FillParams(ASQLCommand: TDBXCommand; args: OleVariant);
+var
+  LParamPos: Integer;
+  LIsCreated: Boolean;
+  LParam: TDBXParameter;
+begin
+  LIsCreated := ASQLCommand.Parameters.Count > 0;
+  //FCommand.Parameters.SetCount((VarArrayHighBound(args, 1) + 1)); // Only set, but it's null!
+  if not(VarIsNull(args)) and not(VarIsEmpty(args)) then
+    for LParamPos := 0 to (VarArrayHighBound(args, 1)) do
+    begin
+      if not LIsCreated then
+        LParam := ASQLCommand.CreateParameter
+      else
+        LParam := ASQLCommand.Parameters[LParamPos];
+      case VarType(args[LParamPos]) and VarTypeMask of  {$REGION 'begin..end'}
+        varInteger:begin
+            if not (LParam.DataType=TDBXDataTypes.Int32Type) then LParam.DataType := TDBXDataTypes.Int32Type;
+            LParam.Value.AsInt32 := args[LParamPos];
+          end;
+        varOleStr, varString, varUString:begin
+            if not (LParam.DataType=TDBXDataTypes.WideStringType) then LParam.DataType := TDBXDataTypes.WideStringType;
+            LParam.Value.AsString := args[LParamPos];
+          end;
+        varBoolean:begin
+            if not (LParam.DataType=TDBXDataTypes.BooleanType) then LParam.DataType := TDBXDataTypes.BooleanType;
+            LParam.Value.AsBoolean := args[LParamPos];
+          end;
+        varDouble:begin
+            if not (LParam.DataType=TDBXDataTypes.DoubleType) then LParam.DataType := TDBXDataTypes.DoubleType;
+            LParam.Value.AsDouble := args[LParamPos];
+          end;
+        varDate:begin
+            if not (LParam.DataType=TDBXDataTypes.DateTimeType) then LParam.DataType := TDBXDataTypes.DateTimeType;
+            LParam.Value.AsDateTime := args[LParamPos];
+          end;
+        varSmallint:begin
+            if not (LParam.DataType=TDBXDataTypes.Int16Type) then LParam.DataType := TDBXDataTypes.Int16Type;
+            LParam.Value.AsInt16 := args[LParamPos];
+          end;
+        varSingle:begin
+            if not (LParam.DataType=TDBXDataTypes.SingleType) then LParam.DataType := TDBXDataTypes.SingleType;
+            LParam.Value.AsSingle := args[LParamPos];
+          end;
+        varCurrency:begin
+            if not (LParam.DataType=TDBXDataTypes.CurrencyType) then LParam.DataType := TDBXDataTypes.CurrencyType;
+            LParam.Value.AsCurrency := args[LParamPos];
+          end;
+        varShortInt:begin
+            if not (LParam.DataType=TDBXDataTypes.Int8Type) then LParam.DataType := TDBXDataTypes.Int8Type;
+            LParam.Value.AsInt8 := args[LParamPos];
+          end;
+        varWord:begin
+            if not (LParam.DataType=TDBXDataTypes.UInt16Type) then LParam.DataType := TDBXDataTypes.UInt16Type;
+            LParam.Value.AsUInt16 := args[LParamPos];
+          end;
+        varInt64:begin
+            if not (LParam.DataType=TDBXDataTypes.Int64Type) then LParam.DataType := TDBXDataTypes.Int64Type;
+            LParam.Value.AsInt64 := args[LParamPos];
+          end;
+      else
+        raise Exception.Create('Invalid Type');
+      end;{$ENDREGION}
+      if not LIsCreated then
+        ASQLCommand.Parameters.AddParameter(LParam);
+    end;
+end;
 
 { Factory }
 
@@ -67,7 +139,7 @@ var
 begin
   Cmd := GetConnection.CreateCommand;
   try
-    Cmd.Text := ASQL;
+    Cmd.Text := SET_NOCOUNT_ON + ASQL; // fixed: Cursor not returned from Query
     Cmd.Prepare;
   except
     FreeAndNil(Cmd);
@@ -121,90 +193,14 @@ end;
 function TDBXMSSQLFactory.Execute(ASQL: string; args: OleVariant): Int64;
 var
   LDBXCmd: TDBXCommand;
-  LParamPos: Integer;
-  LParam: TDBXParameter;
-  LIsCreated: Boolean;
 begin
   Result := -1;
   try
     LDBXCmd := GetConnection.CreateCommand;
     LDBXCmd.CommandType := TDBXCommandTypes.DbxSQL;
     LDBXCmd.Text := ASQL;
-    //if not LDBXCmd.IsPrepared then
-      LDBXCmd.Prepare;
-    LIsCreated := LDBXCmd.Parameters.Count > 0;
-    //FCommand.Parameters.SetCount((VarArrayHighBound(args, 1) + 1)); // Only set, but it's null!
-    if not(VarIsNull(args)) and not(VarIsEmpty(args)) then
-      for LParamPos := 0 to (VarArrayHighBound(args, 1)) do
-      begin
-        if not LIsCreated then
-          LParam := LDBXCmd.CreateParameter
-        else
-          LParam := LDBXCmd.Parameters[LParamPos];
-        case VarType(args[LParamPos]) and VarTypeMask of  {$REGION 'begin..end'}
-          varInteger:
-            begin
-              LParam.DataType := TDBXDataTypes.Int32Type;
-              LParam.Value.AsInt32 := args[LParamPos];
-            end;
-          varOleStr, varString, varUString:
-            begin
-              LParam.DataType := TDBXDataTypes.WideStringType;
-              LParam.Value.AsString := args[LParamPos];
-            end;
-          varBoolean:
-            begin
-              LParam.DataType := TDBXDataTypes.BooleanType;
-              LParam.Value.AsBoolean := args[LParamPos];
-            end;
-          varDouble:
-            begin
-              LParam.DataType := TDBXDataTypes.DoubleType;
-              LParam.Value.AsDouble := args[LParamPos];
-            end;
-          varDate:
-            begin
-              LParam.DataType := TDBXDataTypes.DateTimeType;
-              LParam.Value.AsDateTime := args[LParamPos];
-            end;
-          varSmallint:
-            begin
-              LParam.DataType := TDBXDataTypes.Int16Type;
-              LParam.Value.AsInt16 := args[LParamPos];
-            end;
-          varSingle:
-            begin
-              LParam.DataType := TDBXDataTypes.SingleType;
-              LParam.Value.AsSingle := args[LParamPos];
-            end;
-          varCurrency:
-            begin
-              LParam.DataType := TDBXDataTypes.CurrencyType;
-              LParam.Value.AsCurrency := args[LParamPos];
-            end;
-          varShortInt:
-            begin
-              LParam.DataType := TDBXDataTypes.Int8Type;
-              LParam.Value.AsInt8 := args[LParamPos];
-            end;
-          varWord:
-            begin
-              LParam.DataType := TDBXDataTypes.UInt16Type;
-              LParam.Value.AsUInt16 := args[LParamPos];
-            end;
-          varInt64:
-            begin
-              LParam.DataType := TDBXDataTypes.Int64Type;
-              LParam.Value.AsInt64 := args[LParamPos];
-            end;
-        else
-          raise Exception.Create('Invalid Type');
-        end;{$ENDREGION}
-        if not LIsCreated then
-          LDBXCmd.Parameters.AddParameter(LParam);
-      end;
-    LDBXCmd.ExecuteUpdate;
-    Result := LDBXCmd.RowsAffected;
+    LDBXCmd.Prepare;
+    Result := Self.Execute(LDBXCmd, args);
   finally
     FreeAndNil(LDBXCmd);
   end;
@@ -222,95 +218,38 @@ begin
   Result := FDBXConnection;
 end;
 
-function TDBXMSSQLFactory.Prepare(ASQL: string; args: OleVariant): TDBXCommand;
+function TDBXMSSQLFactory.Execute(ASQLCommand: TDBXCommand;
+  args: OleVariant): Int64;
+begin
+  if not ASQLCommand.IsPrepared then
+    ASQLCommand.Prepare;
+  FillParams(ASQLCommand, args);
+  ASQLCommand.ExecuteUpdate;
+  Result := ASQLCommand.RowsAffected;
+end;
+
+function TDBXMSSQLFactory.ExecuteQuery(ASQLCommand: TDBXCommand;
+  args: OleVariant): TDBXReader;
+begin
+  if not ASQLCommand.IsPrepared then
+    ASQLCommand.Prepare;
+  FillParams(ASQLCommand, args);
+  Result := ASQLCommand.ExecuteQuery;
+end;
+
+function TDBXMSSQLFactory.ExecuteQuery(ASQL: string;
+  args: OleVariant): TDBXReader;
 var
   LDBXCmd: TDBXCommand;
-  LParam: TDBXParameter;
-  LParamPos: Integer;
-  LIsCreated: Boolean;
 begin
-  Result := nil;
-  LDBXCmd := GetConnection.CreateCommand;
   try
-    try
-      LDBXCmd.Text := ASQL;
-      LDBXCmd.Prepare;
-      LIsCreated := LDBXCmd.Parameters.Count > 0;
-      if not(VarIsNull(args)) and not(VarIsEmpty(args)) then
-        for LParamPos := 0 to (VarArrayHighBound(args, 1)) do
-        begin
-          if not LIsCreated then
-            LParam := LDBXCmd.CreateParameter
-          else
-            LParam := LDBXCmd.Parameters[LParamPos];
-          case VarType(args[LParamPos]) and VarTypeMask of {$REGION 'begin..end'}
-            varInteger:
-              begin
-                LParam.DataType := TDBXDataTypes.Int32Type;
-                LParam.Value.AsInt32 := args[LParamPos];
-              end;
-            varOleStr, varString, varUString:
-              begin
-                LParam.DataType := TDBXDataTypes.WideStringType;
-                LParam.Value.AsString := args[LParamPos];
-              end;
-            varBoolean:
-              begin
-                LParam.DataType := TDBXDataTypes.BooleanType;
-                LParam.Value.AsBoolean := args[LParamPos];
-              end;
-            varDouble:
-              begin
-                LParam.DataType := TDBXDataTypes.DoubleType;
-                LParam.Value.AsDouble := args[LParamPos];
-              end;
-            varDate:
-              begin
-                LParam.DataType := TDBXDataTypes.DateTimeType;
-                LParam.Value.AsDateTime := args[LParamPos];
-              end;
-            varSmallint:
-              begin
-                LParam.DataType := TDBXDataTypes.Int16Type;
-                LParam.Value.AsInt16 := args[LParamPos];
-              end;
-            varSingle:
-              begin
-                LParam.DataType := TDBXDataTypes.SingleType;
-                LParam.Value.AsSingle := args[LParamPos];
-              end;
-            varCurrency:
-              begin
-                LParam.DataType := TDBXDataTypes.CurrencyType;
-                LParam.Value.AsCurrency := args[LParamPos];
-              end;
-            varShortInt:
-              begin
-                LParam.DataType := TDBXDataTypes.Int8Type;
-                LParam.Value.AsInt8 := args[LParamPos];
-              end;
-            varWord:
-              begin
-                LParam.DataType := TDBXDataTypes.UInt16Type;
-                LParam.Value.AsUInt16 := args[LParamPos];
-              end;
-            varInt64:
-              begin
-                LParam.DataType := TDBXDataTypes.Int64Type;
-                LParam.Value.AsInt64 := args[LParamPos];
-              end;
-          else
-            raise Exception.Create('Invalid Type');
-          end;{$ENDREGION}
-          if not LIsCreated then
-            LDBXCmd.Parameters.AddParameter(LParam);
-        end;
-        Result := LDBXCmd;
-    finally
-    end;
-  except
+    LDBXCmd := GetConnection.CreateCommand;
+    LDBXCmd.CommandType := TDBXCommandTypes.DbxSQL;
+    LDBXCmd.Text := ASQL;
+    LDBXCmd.Prepare;
+    Result := Self.ExecuteQuery(LDBXCmd, args);
+  finally
     FreeAndNil(LDBXCmd);
-    raise;
   end;
 end;
 
